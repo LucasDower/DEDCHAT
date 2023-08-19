9707fca70f2503253915c0e1ce670bd85cf4ec34
Lucas Dower <lucasjdower@gmail.com>
2023-08-19 05:15:17 +0100

    Added box to input chat server address

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -9,27 +9,8 @@ export default function Home() {
     const [gameState, setGameState] = useState<ChatStateSync>();
     const [chat, setChat] = useState<ChatMessage[]>([]);
     const [message, setMessage] = useState<string>();
-
-    useEffect(() => {
-        setChat([]);
-        socket = new WebSocket("ws://localhost:8080");
-
-        // Connection opened
-        socket.addEventListener("open", (event) => {
-            console.log('Connection opened');
-        });
-
-        // Listen for messages
-        socket.addEventListener("message", (event) => {
-            const newGameState: ChatStateSync = JSON.parse(event.data); // TODO: Naughty
-            setGameState(newGameState);
-            setChat(chat => [...chat, ...newGameState.messages]);
-        });
-
-        return () => {
-            socket?.close();
-        }
-    }, []);
+    const [connected, setConnected] = useState(false);
+    const [address, setAddress] = useState<string>();
 
     const handleSendMessage = (message: string) => {
         if (message.startsWith('/clear')) {
@@ -47,10 +28,45 @@ export default function Home() {
         }
     };
 
+    const handleAttemptConnection = (address: string) => {
+        socket?.close();
+        socket = null;
+        setConnected(false);
+
+        setChat([]);
+        socket = new WebSocket("ws://" + address);
+
+        // Connection opened
+        socket.addEventListener("open", (event) => {
+            console.log('Connection opened');
+            setConnected(true);
+        });
+
+        socket.addEventListener('close', (event) => {
+            setConnected(false);
+        })
+
+        // Listen for messages
+        socket.addEventListener("message", (event) => {
+            const newGameState: ChatStateSync = JSON.parse(event.data); // TODO: Naughty
+            setGameState(newGameState);
+            setChat(chat => [...chat, ...newGameState.messages]);
+        });
+    }
+
+    const handleAddressKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleAttemptConnection(address!);
+        }
+    };
+
     return (
         <div className="p-16 bg-gray-50 space-x-5 min-h-screen flex  flex-row">
             <div className="bg-white p-5 rounded-lg border space-y-3 flex-grow flex flex-col">
-                <p>Chat</p>
+                <p>Chat ({ connected ? 'Connected' : 'Disconnected' })</p>
+                <input className="border p-4 flex bg-gray-100" value={ address} onChange={(e) => { setAddress(e.target.value) }} placeholder='localhost:8080' onKeyDown={handleAddressKeyDown}>
+                </input>
                 <div className="border flex flex-col bg-gray-100 flex-grow">
                     {chat.map(x => x.sender.type === 'server'
                         ? <div className='font-mono text-gray-400' key={x.uuid}>[SERVER]: {x.message}</div>
